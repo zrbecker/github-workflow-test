c3c5d5ae9fbbc8bd02411b605dcb649809ca4989
Zachary Becker <zrbecker@gmail.com>
2017-10-19 17:52:01 -0700

    put print in it's own function

--- projects/hello_world/hello_world.py
+++ projects/hello_world/hello_world.py
@@ -1,5 +1,12 @@
 """This module simply prints "Hello World!".
 """
 
-if __name__ == '__main__':
+def say_hello():
+    """Prints "Hello World!" to the stdout.
+    >>> say_hello()
+    Hello World!
+    """
     print('Hello World!')
+
+if __name__ == '__main__':
+    say_hello()
